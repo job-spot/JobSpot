30fed863a120b3cb1cf9ca7dcc44cd1d3d2e146a
Cindy Chau <117945543+cindychau1@users.noreply.github.com>
2023-04-03 18:24:36 -0400

    added job api routes

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,3 +1,28 @@
 //import express
 import express from 'express';
+import { jobController } from '../controllers/jobController';
 export const jobRouter = express.Router();
+
+// GET JOB
+// http://localhost:3000/api/job
+router.get('/job', jobController.getJob, (req, res) => {
+  return res.status(200).json(res.locals.getJobs);
+});
+
+// ADD JOB
+// http://localhost:3000/api/job
+router.post('/job', jobController.addJob, (req, res) => {
+  return res.status(200).json(res.locals.newJob);
+});
+
+// Update JOB
+// http://localhost:3000/api/job
+router.post('/job', jobController.updateJob, (req, res) => {
+  return res.status(200).json(res.locals.updatedJob);
+});
+
+// DELETE JOB
+// http://localhost:3000/api/job
+router.delete('/job', jobController.deleteJob, (req, res) => {
+  return res.status(200).json(res.locals.deletedJob);
+});
