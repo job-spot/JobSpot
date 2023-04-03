de29c12fe991a129496154fee5dd7dc47f82d118
Cindy Chau <117945543+cindychau1@users.noreply.github.com>
2023-04-03 19:58:04 -0400

    fixed bug with routes on api.js

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,27 +1,27 @@
 import express from 'express';
-import { jobController } from '../controllers/jobController.js';
 export const jobRouter = express.Router();
+import { jobController } from '../controllers/jobController.js';
 
 // GET JOB
 // http://localhost:3000/api/job
-router.get('/job', jobController.getJob, (req, res) => {
+jobRouter.get('/job', jobController.getJob, (req, res) => {
   return res.status(200).json(res.locals.getJobs);
 });
 
 // ADD JOB
 // http://localhost:3000/api/job
-router.post('/job', jobController.addJob, (req, res) => {
+jobRouter.post('/job', jobController.addJob, (req, res) => {
   return res.status(200).json(res.locals.newJob);
 });
 
 // Update JOB
 // http://localhost:3000/api/job
-router.post('/job', jobController.updateJob, (req, res) => {
-  return res.status(200).json(res.locals.updatedJob);
-});
+// jobRouter.put('/job', jobController.updateJob, (req, res) => {
+//   return res.status(200).json(res.locals.updatedJob);
+// });
 
 // DELETE JOB
 // http://localhost:3000/api/job
-router.delete('/job', jobController.deleteJob, (req, res) => {
+jobRouter.delete('/job', jobController.deleteJob, (req, res) => {
   return res.status(200).json(res.locals.deletedJob);
 });
