83fb401fc6e6410f9a3887aa5994a9a4c8c47caa
vctorye <vctorye@gmail.com>
2023-04-03 13:17:30 -0400

    boiler plate

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { useState } from 'react'
+import "./styles/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JobSpot</h1>
+    </div>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    }
+  }
 })
