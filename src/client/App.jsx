aef5f2ee7fa4e888cd145ca5e54f2f591241feb7
Jen Lee <jenlee@cca.edu>
2023-04-04 13:04:50 -0700

    jen made new branch

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,10 @@
 import * as React from "react"
-import Login from "./components/Login"
-import Signup from "./components/Signup"
 import "./styles.module.css"
 
 function App() {
   return (
     <div className="App">
       <h1>JobSpot</h1>
-      <Login />
-      <Signup />
     </div>
   )
 }
